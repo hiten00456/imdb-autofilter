3b7a05d897e7273bd862b96d5ba85f3ab2ea23cc
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-10-17 16:17:31 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,10 +89,10 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Main Channel', url='t.me/tn_linkZz'),
+        InlineKeyboardButton('support Group', url='t.me/powerrockers')
         ],[
-        InlineKeyboardButton('🖥️ Deploy Video 🖥️', url=f'{TUTORIAL}')
+        InlineKeyboardButton('Updates Channel', url=t.me/tnrockers2021)
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
