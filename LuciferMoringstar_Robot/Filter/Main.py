9078924b1481341a41259cefccf835b26156571b
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-10-18 09:46:58 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"🎥 𝐓𝐢𝐭𝐥𝐞 : {search}\n🎙 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : Tamil \n⚜ 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 : HDRip \n⭐ 𝐑𝐚𝐭𝐢𝐧𝐠 :  {random.choice(RATING)}\n🎭 𝐆𝐞𝐧𝐫𝐞 :  {random.choice(GENRES)}\n📤 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫 : {message.chat.title}""
+        mo_tech_yt = f""🎥 𝐓𝐢𝐭𝐥𝐞 : {search}\n🎙 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : Tamil \n⚜ 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 : HDRip \n⭐ 𝐑𝐚𝐭𝐢𝐧𝐠 :  {random.choice(RATING)}\n🎭 𝐆𝐞𝐧𝐫𝐞 :  {random.choice(GENRES)}\n📤 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫 : {message.chat.title}""
         files = await get_filter_results(query=search)
         if files:
             for file in files:
