89290718f3d67c53f8b533c416995919858a2d7f
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-10-19 11:06:20 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -129,9 +129,6 @@ async def group(client, message):
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
                 )
-               
-
-           )
             return
         if not btn:
             return
