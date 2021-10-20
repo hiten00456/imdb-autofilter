3ab97b5c57782ece129dce3bd1e3860e8ba1f059
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-10-20 18:54:46 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -128,9 +128,8 @@ async def group(client, message):
                 filename = f"🎬[{get_size(file.file_size)}] 📁{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
-                ,
-                 reply_to_message_id=message.message_id
               )
+          else:
             return
         if not btn:
             return
